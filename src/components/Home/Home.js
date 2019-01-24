0ad8a3183c6ed7f5a5e75b2420e9f990a1e79d21
justinandjesus <justinandjesus@gmail.com>
2019-01-24 17:53:44 +0800

    converted code

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,25 +5,46 @@ import './loading.js';
 
 
 class Home extends React.Component {
-  componentWillMount(){
-    
-  }
-  render() {
-    return (
-        <div>
-          <div class={classes.LoadingPage}>
-            <div class={classes.Counter}>
-              <p>LOADING</p>
-              <h1>0%</h1>
-              <hr/>
+    componentDidMount() {
+
+        $(document).ready(function () {
+            var counter = 0;
+            var c = 0;
+            var perfData = window.performance.timing // The PerformanceTiming interface
+            var estimatedTime = -(perfData.loadEventEnd - perfData.navigationStart) // Calculated Estimated Time of Page Load which returns negative value.
+            var time = parseInt((estimatedTime / 1000) % 60) * 100; //Converting EstimatedTime from miliseconds to seconds.
+            var i = setInterval(function () {
+                $("#percentageCounter").html(c + "%");
+                $("#percentageCounterHr").css("width", c + "%");
+                $("#percentageCounterHr").css("background", "linear-gradient(to right, #f60d54 "+ c + "%,#0d0d0d "+ c + "%)");
+
+                counter++;
+                c++;
+
+                if (counter == 101) {
+                    clearInterval(i);
+                }
+            }, estimatedTime / 100);
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={classes.LoadingPage}>
+                    <div className={classes.Counter}>
+                        <p>LOADING</p>
+                        <h1 id="percentageCounter">0%</h1>
+                        <hr id="percentageCounterHr" />
+                    </div>
+                </div>
+                <div id="home" className={classes.ShadowOverlay}>
+                    Shikhar
+                </div>
             </div>
-          </div>
-          <div id={classes.home} className={classes.ShadowOverlay}>
-              Shikhar
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Home;
